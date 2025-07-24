0e84f3c3a512df1a866f5b7363affb9d7508e401
Jake Smith <jakethegreatest93@gmail.com>
2025-07-24 12:22:30 +0100

    add base html

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Race, Background, Alignment, Character
 
-# Register your models here.
+admin.site.register(Race)
+admin.site.register(Background)
+admin.site.register(Alignment)
+admin.site.register(Character)
